6d150ef4dbf96f40a8c6e1c704afb98b3cf3deb6
22021990 <salapurastefan@yahoo.com>
2020-11-21 20:53:46 +0100

    Dodati fajlovi

---
+++ htec/src/main/java/com/stefan/salapura/htec/pojo/Airport.java
@@ -0,0 +1,26 @@
+package com.stefan.salapura.htec.pojo;
+
+/*
+ * 1739,"Belgrade Nikola Tesla Airport","Belgrade","Serbia","BEG","LYBE",44.8184013367,20.3090991974,335,1,"E","Europe/Belgrade","airport","OurAirports"
+ * 
+ * Field:					Value:								Description:
+ * 
+ * Airport ID				1739
+ * Name						"Belgrade Nikola Tesla Airport"
+ * City						"Belgrade"
+ * Country					"Serbia"
+ * IATA						"BEG"                        		3-letter IATA code. Null if not assigned/unknown.
+ * ICAO						"LYBE"								4-letter ICAO code. Null if not assigned.					
+ * Latitude					44.818401336
+ * Longitude				20.3090991974
+ * Altitude					335
+ * Timezone					1									Hours offset from UTC.
+ * DST						"E"									Daylight saving time. One of E (Europe), A (US/Canada), S (South America), O (Australia), Z (New Zealand), N (None) or U (Unknown).
+ * Tz database time zone	"Europe/Belgrade"					Timezone in "tz" (Olson) format, eg. "America/Los_Angeles".
+ * Type						"airport"
+ * Source					"OurAirports"
+ */
+
+public class Airport {	
+
+}
---
+++ htec/src/main/java/com/stefan/salapura/htec/pojo/Route.java
@@ -0,0 +1,22 @@
+package com.stefan.salapura.htec.pojo;
+
+/*
+ * 2B,410,AER,2965,KZN,2990,,0,CR2,95.87
+ * 
+ * Field:					Value:		Description:
+ * 
+ * Airline  				2B			2-letter (IATA) or 3-letter (ICAO) code of the airline.
+ * Airline ID 				410
+ * Source airport			AER			3-letter (IATA) or 4-letter (ICAO) code of the source airport.
+ * Source airport ID		2965		Identifier for source airport.
+ * Destination airport		KZN			3-letter (IATA) or 4-letter (ICAO) code of the destination airport.
+ * Destination airport ID	2990	
+ * Codeshare							"Y" if this flight is a codeshare, empty otherwise.
+ * Stops					0			Number of stops on this flight ("0" for direct).
+ * Equipment				CR2			3-letter codes for plane type(s) generally used on this flight, separated by spaces.
+ * Price					95.87		Flight cost.
+ */
+
+public class Route {
+
+}
